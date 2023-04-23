67d49604a072c2a86743c18d99c467db6615f5cf
Lucas Daddiego <daddiego.lucas@gmail.com>
2023-04-23 03:09:10 -0300

    Lucio's Songs

--- js/metronome.js
+++ js/metronome.js
@@ -15,14 +15,19 @@ const songTable = document.getElementById('songsTable');
 const tempo = document.getElementById('tempo');
 const timerWorker = new Worker('js/worker.js');
 const songList = [
-    ['Verte Así', 140],
-    ['Que se yo qué hacer', 138],
-    ['El Payaso', 120],
-    ['Macumba', 95],
-    ['Pampa y la Via', 156],
-    ['En la Calle', 150],
-    ['Bailando al cielo', 150],
-    ['Sabor a danza', 150]
+    ['Como un Lobo', 130],
+    ['Farmacia', 130],
+    ['Dame', 180],
+    ['Todavia', 110],
+    ['Pasan las horas', 122],
+    ['Tan Distintos', 120],
+    ['Infierno Blanco', 115],
+    ['Reyes de la Noche', 140],
+    ['Hay Momentos', 94],
+    ['Espejo Roto', 129],
+    ['100 Años', 139],
+    ['Me Muero', 125],
+    ['Gracias', 138]
 ]
 const songListLength = songList.length;
 
